965e24203200b35dbefcd68da34aba941bedb9bf
ayushr2 <ayushr2@illinois.edu>
2018-11-24 21:26:33 -0600

    update script

--- utils/start_run_script.py
+++ utils/start_run_script.py
@@ -1,31 +1,28 @@
 import json
+import requests
 import sys
 
-from tornado import httpclient
-import utils.grading_run
-
-import src.constants.api_keys as api_key
-from src.config import GRADING_RUN_ENDPOINT
+HOST = "fa18-cs241-437.cs.illinois.edu"
+PORT = "1470"
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python start_run_script.py <HOST> <PORT> <cluster token>")
+    if len(sys.argv) != 3:
+        print("Usage: python start_run_script.py <path to json config> <token>")
         exit(-1)
+    token = sys.argv[2]
+    with open(sys.argv[1]) as f:
+        headers = {"Authorization": token}
+        config = json.load(f)
+        r = requests.post("http://{}:{}/api/v1/grading_run".format(HOST, PORT), headers=headers,
+                          data=json.dumps(config))
+        if r.status_code != 200:
+            print("Error in uploading config: {}".format(r.text))
+            exit(-1)
 
-    HOST = sys.argv[1]
-    PORT = sys.argv[2]
-    token = sys.argv[3]
+        id_ = json.loads(r.text)["data"]["grading_run_id"]
+        r = requests.post("http://{}:{}/api/v1/grading_run/{}".format(HOST, PORT, id_), headers=headers)
+        if r.status_code != 200:
+            print("Error in starting run: {}".format(r.text))
+            exit(-1)
 
-    def get_url(endpoint):
-        return "http://{}:{}{}".format(HOST, PORT, endpoint)
-
-
-    headers = {api_key.AUTH: token}
-    http_client = httpclient.HTTPClient()
-    add_request = httpclient.HTTPRequest(get_url(GRADING_RUN_ENDPOINT), method="POST",
-                                         body=json.dumps(utils.grading_run.valid_json))
-    response = http_client.fetch(add_request)
-    id_ = json.loads(response.body)["data"][api_key.RUN_ID]
-    response = http_client.fetch("{}/{}".format(get_url(GRADING_RUN_ENDPOINT), id_), method='POST', headers=headers,
-                                 body="")
-    print("Grading run with id {} started!".format(id_))
+        print("Grading run with id {} started!".format(id_))

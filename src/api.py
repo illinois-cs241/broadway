1848cbb3b6af138d99b664b4886a404d9c149d46
ayushr2 <ayushr2@illinois.edu>
2018-12-28 00:23:00 +0530

    FINALLY HANDLED DEAD WORKER NODES

--- src/api.py
+++ src/api.py
@@ -2,8 +2,10 @@ import json
 import logging
 import os
 import signal
+import time
 from logging.handlers import TimedRotatingFileHandler
 from queue import Queue
+from threading import Thread, Condition
 
 import tornado
 import tornado.ioloop
@@ -30,9 +32,14 @@ logging.basicConfig(
     level=logging.INFO
 )
 logger = logging.getLogger()
+heartbeat_running = True
+heartbeat_cv = Condition()
 
 
 def shutdown():
+    global heartbeat_running
+    heartbeat_running = False
+
     tornado.ioloop.IOLoop.current().stop()
     db_resolver = app.settings.get(consts.APP_DB)  # type: DatabaseResolver
     db_resolver.shutdown()
@@ -45,21 +52,22 @@ def signal_handler(sig, frame):
 def handle_lost_worker_node(worker_node):
     running_job_id = worker_node.get(db_key.RUNNING_JOB)
     worker_id = str(worker_node.get(db_key.ID))
-    logging.critical("Grader {} executing {} went offline unexpectedly".format(worker_id, running_job_id))
+    logger.critical("Grader {} executing {} went offline unexpectedly".format(worker_id, running_job_id))
 
     if running_job_id is None:
         return
 
     # Make a fake update request on behalf of the dead worker so that the job is marked as failed and the rest of the
     # jobs for the grading run can be handled as expected and scheduled in the right order
-    http_client = httpclient.AsyncHTTPClient()
+    http_client = httpclient.HTTPClient()
     res = {api_key.SUCCESS: False, api_key.RESULTS: [{"result": "Grader died while executing this job"}],
            api_key.LOGS: {"logs": "No logs available for this job since the grader died while executing this job"}}
     update_request = httpclient.HTTPRequest(
         "http://localhost:{}{}/{}".format(PORT, GRADING_JOB_ENDPOINT, running_job_id),
         headers={api_key.AUTH: cluster_token, api_key.WORKER_ID: worker_id},
         method="POST", body=json.dumps(res))
-    yield http_client.fetch(update_request)
+    http_client.fetch(update_request)
+    http_client.close()
 
 
 def heartbeat_validator():
@@ -67,19 +75,24 @@ def heartbeat_validator():
     Checks if any of the grader machines went offline. It decides so if the grader machine has not sent any
     heartbeat in the past 2 X HEARTBEAT_INTERVAL seconds.
     """
-    global app
+    global heartbeat_running
 
-    db_resolver = app.settings.get(consts.APP_DB)  # type: DatabaseResolver
-    worker_nodes_collection = db_resolver.get_worker_node_collection()
+    while heartbeat_running:
+        db_resolver = app.settings.get(consts.APP_DB)  # type: DatabaseResolver
+        worker_nodes_collection = db_resolver.get_worker_node_collection()
 
-    cur_time = get_time()
-    for worker_node in worker_nodes_collection.find():  # type: dict
-        last_seen_time = worker_node.get(db_key.LAST_SEEN)
+        cur_time = get_time()
+        for worker_node in worker_nodes_collection.find():  # type: dict
+            last_seen_time = worker_node.get(db_key.LAST_SEEN)
 
-        # the worker node dead if it does not send a heartbeat for 2 intervals
-        if (cur_time - last_seen_time).total_seconds() >= 2 * HEARTBEAT_INTERVAL:
-            handle_lost_worker_node(worker_node)
-            worker_nodes_collection.delete_one({db_key.ID: worker_node.get(db_key.ID)})
+            # the worker node dead if it does not send a heartbeat for 2 intervals
+            if (cur_time - last_seen_time).total_seconds() >= 2 * HEARTBEAT_INTERVAL:
+                handle_lost_worker_node(worker_node)
+                worker_nodes_collection.delete_one({db_key.ID: worker_node.get(db_key.ID)})
+
+        heartbeat_cv.acquire()
+        heartbeat_cv.wait(HEARTBEAT_INTERVAL)
+        heartbeat_cv.release()
 
 
 def make_app(token, db_object):
@@ -126,5 +139,14 @@ if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
 
     # Checks if any worker node disconnected every HEARTBEAT_INTERVAL seconds.
-    tornado.ioloop.PeriodicCallback(heartbeat_validator, HEARTBEAT_INTERVAL * 1000).start()
+    heartbeat_thread = Thread(target=heartbeat_validator)
+    heartbeat_thread.start()
+
+    # start event loop
     tornado.ioloop.IOLoop.instance().start()
+
+    # shutdown heartbeat thread cleanly
+    heartbeat_cv.acquire()
+    heartbeat_cv.notify_all()
+    heartbeat_cv.release()
+    heartbeat_thread.join()

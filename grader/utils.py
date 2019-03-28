0c7f5ac6a4a65f6634bf1dc5ac96fc6f85913d99
rod-lin <zl38@illinois.edu>
2019-03-28 10:28:16 -0500

    fix utils.py

--- grader/utils.py
+++ grader/utils.py
@@ -2,7 +2,7 @@ from config import API_HOSTNAME, API_PORT, API_PROXY
 
 
 def get_url(endpoint):
-    return "http://{}:{}{}{}".format(API_HOSTNAME, API_PORT, API_PROXY, endpoint)
+    return "https://{}:{}{}{}".format(API_HOSTNAME, API_PORT, API_PROXY, endpoint)
 
 
 def print_usage():

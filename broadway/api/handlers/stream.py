0c8035f1683224ff21237cfa7bdff409b2ab22ef
Jack Henhapl <46969347+jhenhapl@users.noreply.github.com>
2021-03-10 18:31:42 -0600

    Making updates job specific (#37)
    * Making updates job specific
    
    * Adding heartbeat
    
    * Sending empty message as heartbeat
    
    * Making callback async
    
    * Not sending heartbeat if callback is not running
    
    * Directly sending message
    
    * Making all job types streamable
    
    * Adding sse helper
    
    * Removing commented out code
    
    * Changing constant name

--- broadway/api/handlers/stream.py
+++ broadway/api/handlers/stream.py
@@ -1,10 +1,13 @@
 import json
 from tornado import gen, web
 from tornado.iostream import StreamClosedError
+from tornado.ioloop import PeriodicCallback
 
 from broadway.api.handlers.base import BaseAPIHandler
 from broadway.api.decorators.auth import authenticate_course_member_or_admin
 from broadway.api.utils.streamqueue import StreamQueue
+
+HEARTBEAT_TIME_MILLI = 20 * 1000  # 20 seconds
 
 
 class GradingJobStreamHandler(BaseAPIHandler):
@@ -14,19 +17,32 @@ class GradingJobStreamHandler(BaseAPIHandler):
         self.set_header("Cache-Control", "no-cache")
 
         self._id = id(self)
+        self._callback = PeriodicCallback(
+            callback=self._heartbeat, callback_time=HEARTBEAT_TIME_MILLI
+        )
+        self._callback.start()
 
     def _stop_listening(self):
         self.get_stream_queue().unregister_stream(self._job_id, self._id)
+        self._callback.stop()
         raise web.Finish
 
     @gen.coroutine
-    def publish(self, event, data):
+    def _send_sse(self, message):
         try:
-            blob = json.dumps({"type": event, "data": data})
-            self.write(f"event: status_update\ndata: {blob}\n\n")
+            self.write(message)
             yield self.flush()
         except StreamClosedError:
             self._stop_listening()
+
+    @gen.coroutine
+    def _heartbeat(self):
+        yield self._send_sse(":\n\n")
+
+    @gen.coroutine
+    def publish(self, event, data):
+        blob = json.dumps({"type": event, "data": data})
+        yield self._send_sse(f"event: status_update\ndata: {blob}\n\n")
 
     @authenticate_course_member_or_admin
     @gen.coroutine
